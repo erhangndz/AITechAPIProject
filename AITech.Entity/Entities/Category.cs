501710cfd5dea3d8281e834d236e925743aab25f
erhangndz <erhangndz@live.com>
2025-11-18 22:33:50 +0300

    Add project files.

---
+++ AITech.Entity/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using AITech.Entity.Entities.Common;
+
+namespace AITech.Entity.Entities
+{
+    public class Category: BaseEntity
+    {
+        public string Name { get; set; }
+
+        public IList<Project> Projects { get; set; }
+
+
+    }
+}
