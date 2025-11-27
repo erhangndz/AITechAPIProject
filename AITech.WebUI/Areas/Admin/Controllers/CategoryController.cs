62723aa456a0b911566e61905a49edf39c8186f3
erhangndz <erhangndz@live.com>
2025-11-27 21:48:21 +0300

    updated

---
+++ AITech.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using AITech.WebUI.DTOs.CategoryDtos;
+using AITech.WebUI.Services.CategoryServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AITech.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController(ICategoryService _categoryService) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            return View(categories);
+        }
+
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto categoryDto)
+        {
+            await _categoryService.CreateAsync(categoryDto);
+            return RedirectToAction("Index");
+        }
+    }
+}
